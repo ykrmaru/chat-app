52c1931472c8ea14b80cd588f6f4f0d09c9df895
Yukari Maruyama <ykrmaruyama0323@gmail.com>
2026-07-29 10:15:12 +0900

    fix: scroll message list after rendered chat items

--- app/components/chat/ChatMessageList.vue
+++ app/components/chat/ChatMessageList.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { nextTick, ref, watch } from "vue";
 
-const { messages, chatItems } = useChat();
+const { chatItems } = useChat();
 
 const messageListRef = ref<HTMLElement | null>(null);
 
 watch(
-  () => messages.value.length,
+  () => chatItems.value.length,
   async () => {
     await nextTick();
 
@@ -19,7 +19,7 @@ watch(
 </script>
 
 <template>
-  <main ref="messageListRef" class="flex-1 overflow-y-auto p-6">
+  <main ref="messageListRef" class="min-h-0 flex-1 overflow-y-auto p-6">
     <template v-for="item in chatItems" :key="item.type === 'separator'
       ? `separator-${item.date}`
       : item.message.id
